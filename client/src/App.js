2e4a238d90ca3cab705e4ce7d30c9808ce452059
Harsh <57341907+harsh-dexter@users.noreply.github.com>
2024-08-23 12:10:25 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,47 @@
-import './App.css';
-import Post from "./Post";
-import Header from "./Header";
-import {Route, Routes} from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import {UserContextProvider} from "./UserContext";
+import { UserContextProvider } from "./UserContext";
 import CreatePost from "./pages/CreatePost";
 import PostPage from "./pages/PostPage";
 import EditPost from "./pages/EditPost";
+import DynamicBackground from "./DynamicBackground";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(() => {
+    const savedMode = localStorage.getItem('darkMode');
+    return savedMode ? JSON.parse(savedMode) : false;
+  });
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    localStorage.setItem('darkMode', JSON.stringify(darkMode));
+  }, [darkMode]);
+
   return (
     <UserContextProvider>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<IndexPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/create" element={<CreatePost />} />
-          <Route path="/post/:id" element={<PostPage />} />
-          <Route path="/edit/:id" element={<EditPost />} />
-        </Route>
-      </Routes>
+      <div className="relative min-h-screen">
+        <DynamicBackground darkMode={darkMode} />
+        <div className="relative z-10">
+          <Routes>
+            <Route path="/" element={<Layout darkMode={darkMode} setDarkMode={setDarkMode} />}>
+              <Route index element={<IndexPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/create" element={<CreatePost />} />
+              <Route path="/post/:id" element={<PostPage />} />
+              <Route path="/edit/:id" element={<EditPost />} />
+            </Route>
+          </Routes>
+        </div>
+      </div>
     </UserContextProvider>
   );
 }
